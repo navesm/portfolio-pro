34818eb4869c5ca08504ed631bdb2ac5ad98f30e
Steven NAVE <stevennave@gmail.com>
2025-01-07 20:29:23 -0500

    added Pong project and project card/img

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,11 +4,13 @@ import './Projects.css';
 import focusFitImage from '../../focus-fit.png';
 import spockRockImage from '../../spock-rock.png';
 import calculatorImage from '../../calculator-shot.png';
+import pongImage from '../../pong-shot.png';
 
 const Projects = () => {
   const projects = [
     { title: 'Focus Fit', description: ' React | Supabase. The best dang exercise and study timer out there', link: 'https://focusfitapp.netlify.app', bgColor: "darkblue", imageUrl: focusFitImage },
     { title: 'Scientific Calculator++', description: 'A scientific calculator that features a quadratic equation solver and prime factorization tool!', link: 'https://navesm.github.io/calculator/', bgColor: '#333', imageUrl: calculatorImage },
+    { title: 'Pong', description: 'A reboot of the classic Pong. Multiplayer coming soon!', link: 'https://navesm.github.io/pong/', bgColor: '#398', imageUrl: pongImage },
     { title: 'Spock Rock', description: 'Play versus the computer in Rock, Paper, Scissors, Lizard, Spock!', link: 'https://navesm.github.io/spock-rock/', bgColor: '#049C67', imageUrl: spockRockImage }
   ];
 
